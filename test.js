a3354352b73f349ecadea4f3769293a59bf1cd96
Jonathan Bowers <jonotron@gmail.com>
2016-03-21 17:44:42 -0700

    skip tests with specific year

--- test.js
+++ test.js
@@ -6,8 +6,9 @@ import moment from 'moment'
 const parser = new chrono.Chrono
 parser.refiners.push(wholeMonth)
 
-test('test single month in the past', t => {
-  const ref = new Date('2016-02-03') // Feb 3, 2016
+const ref = new Date('2016-03-15') // March 15, 2016
+
+test('single month in the past', t => {
   const dates = parser.parse('Jan')  
 
   const janStart = moment('2016-01-01')
@@ -18,11 +19,21 @@ test('test single month in the past', t => {
   t.end()
 })
 
-test('test month range in the past', t => {
-  const ref = new Date('2016-03-15') // March 15, 2016
+test('month range in the past', t => {
   const dates = parser.parse('Jan - Feb')  
 
   const janStart = moment('2016-01-01')
+  const febEnd = moment('2016-02-29')
+
+  t.ok(moment(dates[0].start.date()).isSame(janStart, 'day'), 'start jan 1')
+  t.ok(moment(dates[0].end.date()).isSame(febEnd, 'day'), 'end feb 29')
+  t.end()
+})
+
+test.skip('specific year', t => {
+  const dates = parser.parse('Jan 2015 - Feb 2016')
+
+  const janStart = moment('2015-01-01')
   const febEnd = moment('2016-02-29')
 
   t.ok(moment(dates[0].start.date()).isSame(janStart, 'day'), 'start jan 1')
